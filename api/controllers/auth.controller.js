47ee63aa9161cae695f5a0407b8c542d54faddb0
raghu1016 <raghurukel1016@gmail.com>
2024-07-25 00:56:43 +0530

    sign ui done

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import User from '../models/user.model.js'
+import bcryptjs from "bcryptjs"
+
+
+export const signup = async(req,res,next)=>{
+    const {username,email,password} = req.body;
+    const hashPassword = bcryptjs.hashSync(password,10); 
+    console.log(hashPassword);
+    const newUser = new User({username,email,password:hashPassword});
+    try{
+        await newUser.save();
+        res.status(201).json("user created successfully");
+
+    }
+    catch (err){
+        next(err);
+    }
+
+
+} 
--- api/index.js
+++ api/index.js
@@ -1,6 +1,33 @@
 import express from 'express'
+import mogoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
+
+mogoose.connect(process.env.MONGO).then(()=>{
+    console.log("mongo connected");
+}).catch((err)=>{
+    console.log(err);
+});
+
+// app.get("/",(req,res)=>{
+//     res.json({message:"Hello world"});
+// })
+app.use(express.json());
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message||"Internal server error";
+    return res.status(statusCode).json({
+        success:false,
+        message,
+        statusCode,
+    })
+})
 
 app.listen(3000,()=>{
     console.log("server running on port 3000")
