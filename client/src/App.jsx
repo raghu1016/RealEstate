589725f886710cd84fc7bbd15d13a43d097d3375
raghu1016 <raghurukel1016@gmail.com>
2024-07-24 09:41:59 +0530

    Initail commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignOut from './pages/SignOut'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path='/' element ={<Home />}></Route>
+        <Route path='/sign-in' element ={<SignIn />}></Route>
+        <Route path='/sign-up' element ={<SignUp />}></Route>
+        <Route path='/profile' element ={<Profile />}></Route>
+        <Route path='/about' element ={<About />}></Route>
+
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
